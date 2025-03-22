57110ab037c28064e2def32860fed1af3c3ea827
gourigiri <gourinandagiri@gmail.com>
2025-03-22 11:31:58 +0530

    Initial commit with UploadReport feature

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["tesseract.js"], // Prevents Vite from trying to bundle it
-    },
-    chunkSizeWarningLimit: 1000, // Optional: Increase chunk limit to avoid warnings
-  },
-});
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+})
