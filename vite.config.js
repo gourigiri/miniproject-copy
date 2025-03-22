5f6e155c19647cff5b992f282465d36f2134df23
gourigiri <gourinandagiri@gmail.com>
2025-03-22 09:35:04 +0530

    Successful build - ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-    build: {
-      rollupOptions: {
-        external: ["tesseract.js"] // Prevents Vite from trying to bundle it
-      },
-      chunkSizeWarningLimit: 1000, // Optional: Increase chunk limit to avoid warnings
-    }
-})
-
-
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["tesseract.js"], // Prevents Vite from trying to bundle it
+    },
+    chunkSizeWarningLimit: 1000, // Optional: Increase chunk limit to avoid warnings
+  },
+});
